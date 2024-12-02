469461f084cbdd03542dd5ca3d50e5b2741aebad
TameerAmer <tameergti@gmail.com>
2024-12-02 18:38:09 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -4,9 +4,9 @@
 :: add :: before the next row to initialize the repository
 ::exit /b
 
-git init
+::git init
 git add .
 git commit -m "first commit"
-git branch -M main
+git branch -M main/Python Code/OptiVision
 git remote add origin https://github.com/TameerAmer/Capstone-Project/tree/main/Python%20Code/OptiVision
 git push -u origin main
